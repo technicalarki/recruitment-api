00f6293187ce49fb6f6ab144b0ba5c252f986e93
technicalarki <florante.navaja@gmail.com>
2020-03-20 16:15:35 +0800

    Initial commit

---
+++ src/main/java/com/vertere/eis/recruitment/VertereRecruitmentPortalApiApplication.java
@@ -0,0 +1,13 @@
+package com.vertere.eis.recruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertereRecruitmentPortalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VertereRecruitmentPortalApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vertere/eis/recruitment/VertereRecruitmentPortalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vertere.eis.recruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VertereRecruitmentPortalApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
